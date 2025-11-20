96d73d00f0d53d7c4995235ccd37135d05041266
AkoKBIkeda <akikoi1024@gmail.com>
2025-11-21 11:02:26 +1300

    feat(layouts): add Header and MobileMenu components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/app/component/layouts/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Header />
+        <main className="mt-16">{children}</main>
       </body>
     </html>
   );
