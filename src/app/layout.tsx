be49dccbf9720440e27f2234a4fff414acc2f0d6
AkoKBIkeda <akikoi1024@gmail.com>
2025-11-23 08:07:06 +1300

    chore: relocate Header/MobileMenu/Footer to src/components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/app/component/layouts/Header";
-import Footer from "./component/layouts/Footer";
+import Header from "@/components/layouts/Header";
+import Footer from "@/components/layouts/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
